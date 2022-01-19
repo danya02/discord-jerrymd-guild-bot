fbdf1db0f9530319f5d81174228102ee043f7197
Danya Generalov <danya@danya02.ru>
2022-01-19 16:22:13 +0300

    Adding checking for unverified member quarantine.

--- src/consts.rs
+++ src/consts.rs
@@ -14,3 +14,52 @@ pub const GUILD_ID: u64 = 775744109359923221;
 #[cfg(feature="production")]
 pub const GUILD_ID: u64 = 930050496952139867;
 
+#[cfg(not(feature="production"))]
+/// The role ID corresponding to the "unverified" state.
+///
+/// In the guild, there is a verification process:
+/// 1. All users get the "Unverified" role on joining (managed by Dyno)
+/// 2. The "Unverified" role gives write access to:
+///  - a verification channel in which users can send a message to lose the role (managed by Dyno)
+///  - some honeypot channels (they can be written to, but doing so is suspicious)
+/// 3. If a user sends the correct verification message in the correct channel (see
+///    `VERIFY_CHANNEL_ID`), then they lose the role (managed by Dyno)
+/// 4. If a user sends a message to a honeypot channel, or sends the wrong message to the
+///    verification channel, they:
+///    - lose the "Unverified" role
+///    - gain the "Suspicious" role
+///    - a channel is created such that only they have explicit access to it.
+/// 5. If the user loses the "Suspicious" role or leaves while they have that role, the channel is
+///    deleted.
+pub const UNVERIFIED_ROLE_ID: u64 = 933122032835657778;
+
+#[cfg(feature="production")]
+pub const UNVERIFIED_ROLE_ID: u64 = 932565235561287700;
+
+
+#[cfg(not(feature="production"))]
+pub const SUSPICIOUS_ROLE_ID: u64 = 933122346825429054;
+
+#[cfg(feature="production")]
+pub const SUSPICIOUS_ROLE_ID: u64 = 933122675772108860;
+
+
+#[cfg(feature="production")]
+pub const VERIFY_CHANNEL_ID: u64 = 932566212217536543;
+#[cfg(not(feature="production"))]
+pub const VERIFY_CHANNEL_ID: u64 = 933123422488231976;
+
+
+pub const VERIFY_COMMAND: &str = "?member";
+
+
+#[cfg(not(feature="production"))]
+pub const INTERVIEW_CATEGORY: u64 = 933307552521924668;
+
+#[cfg(feature="production")]
+pub const INTERVIEW_CATEGORY: u64 = 933307056155426836;
+
+
+//#[cfg(not(feature="production"))]
+//#[cfg(feature="production")]
+
--- src/on_message.rs
+++ src/on_message.rs
@@ -1,9 +1,35 @@
 use serenity::{
-    model::{channel::Message},
+    model::{channel::Message,
+            id::RoleId,
+            id::GuildId,
+            id::ChannelId,
+            channel::ChannelType,
+            ModelError,
+            Permissions,
+            channel::{PermissionOverwrite, PermissionOverwriteType},
+    },
     prelude::*,
 };
 
+use eyre::{Result, WrapErr};
+
+use crate::consts::*;
+
 pub async fn on_message(ctx: Context, msg: Message) {
+    if let Some(ref member) = msg.member {
+        let unverified_role = RoleId(UNVERIFIED_ROLE_ID);
+        
+        // If the message was sent by a member with an Unverified role, dispatch to the appropriate
+        // function.
+        if member.roles.contains(&unverified_role){
+            match on_message_by_unverified(ctx, msg).await {
+                Err(error) => println!("Error while processing unverified message:\n{}", error),
+                Ok(_) => (),
+        };
+    }
+
+
+/*
     if msg.content == "!ping" {
         // Sending a message can fail, due to a network error, an
         // authentication error, or lack of permissions to post in the
@@ -12,5 +38,60 @@ pub async fn on_message(ctx: Context, msg: Message) {
         if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
             println!("Error sending message: {:?}", why);
         }
+    }*/
     }
 }
+
+async fn on_message_by_unverified(ctx: Context, msg: Message) -> Result<()> {
+    // If the message is the verification command and it is sent in the correct channel,
+    // then ignore it; the other bot will take care of removing the Unverified role.
+    if msg.channel_id == ChannelId(VERIFY_CHANNEL_ID) {
+        if msg.content.starts_with(VERIFY_COMMAND) {
+            println!("Message ok!");
+            return Ok(());
+        }
+    }
+
+    println!("Unverified message {:?} not OK, quarantining user", &msg);
+
+    // If either of the conditions is not met, we want to quarantine the user.
+    // Start by swapping out the Unverified role for the Suspicious role:
+    let unverified_role = RoleId(UNVERIFIED_ROLE_ID);
+    let sus_role = RoleId(SUSPICIOUS_ROLE_ID);
+    let guild_id = GuildId(GUILD_ID);
+
+    let guild = guild_id.to_guild_cached(&ctx.cache).await.ok_or(ModelError::ItemMissing).wrap_err_with(|| format!("Failed to get guild {:?} from cache", guild_id))?;
+    let mut member = guild_id.member(&ctx.http, &msg.author).await.wrap_err_with(|| format!("Failed to get member {:?} from guild {:?}", &msg.author.id, guild))?;
+
+    member.add_role(&ctx.http, sus_role).await.wrap_err_with(|| format!("Failed to add sus role to member {:?}", member))?;
+    member.remove_role(&ctx.http, unverified_role).await.wrap_err_with(|| format!("Failed to remove unverified role from member {:?}", member))?;
+
+    // Now create a channel corresponding to the member ID
+    // and grant the member access to it.
+
+    let interview_channel_name = format!("interview-{}", member.user.id);
+
+    let deny_all = PermissionOverwrite {
+        allow: Permissions::empty(),
+        deny:Permissions::READ_MESSAGES,
+        kind: PermissionOverwriteType::Member(member.user.id),
+    };
+    let allow_member = PermissionOverwrite {
+        allow: Permissions::SEND_MESSAGES | Permissions::READ_MESSAGES,
+        deny: Permissions::empty(),
+        kind: PermissionOverwriteType::Member(member.user.id),
+    };
+    let permissions = vec![deny_all, allow_member];
+
+    guild.create_channel(&ctx.http,
+                         |c| c
+                         .name(interview_channel_name)
+                         .kind(ChannelType::Text)
+                         .category(INTERVIEW_CATEGORY)
+                         .permissions(permissions))
+        .await.wrap_err("Failed to create interview channel")?;
+
+    Ok(())
+
+}
+
